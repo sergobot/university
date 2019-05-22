29e53c951759fdf0054a40f90aba835790d3e907
Sergey Popov <me@sergobot.me>
2019-05-22 12:29:05 +0300

    Implement read_matrix() function
    Signed-off-by: Sergey Popov <me@sergobot.me>

--- cs01/23/matrix_io.c
+++ cs01/23/matrix_io.c
@@ -15,5 +15,17 @@ void print_matrix(matrix mat)
 
 matrix read_matrix(size_t height, size_t width)
 {
-    return create_matrix(height, width);
+    matrix mat = create_matrix(height, width);
+
+    printf("Please enter your (%ld x %ld) matrix in rows:\n", (long) height, (long) width);
+
+    for (size_t i = 0; i < height; ++i) {
+        for (size_t j = 0; j < width; ++j) {
+            if (scanf("%lf", access(mat, i, j)) != 1) {
+                destroy_matrix(&mat);
+                return ERRONEOUS_MATRIX;
+            }
+        }
+    }
+    return mat;
 }
