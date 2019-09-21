9ea61815244d87b8795381fd5a1fa8be925e97d0
Sergey Popov <me@sergobot.me>
2019-09-21 17:33:20 +0300

    Add more methods to the Matrix interface

--- cs02/src/main/java/edu/spbu/matrix/Matrix.java
+++ cs02/src/main/java/edu/spbu/matrix/Matrix.java
@@ -1,23 +1,44 @@
 package edu.spbu.matrix;
 
 /**
- *
+ * Matrix is a mathematical model usually represented by a 2D array of
+ * floating point numbers with some operations defined. This interface requires
+ * a minimal set of these operations.
  */
 public interface Matrix
 {
   /**
    * однопоточное умнджение матриц
    * должно поддерживаться для всех 4-х вариантов
-   * @param o
-   * @return
+   * @param o - another matrix to multiply by (this * o)
+   * @return resulting matrix of the multiplication
    */
   Matrix mul(Matrix o);
 
   /**
    * многопоточное умножение матриц
-   * @param o
-   * @return
+   * @param o - another matrix to multiply by (this * o)
+   * @return resulting matrix of the multiplication
    */
   Matrix dmul(Matrix o);
 
+  /**
+   * Returns an element at specific position
+   * @param i - vertical coordinate of the element
+   * @param j - horizontal coordinate of the element
+   * @return the element at (i, j)
+   */
+  Double get(Integer i, Integer j);
+
+  /**
+   * Returns height of a matrix
+   * @return height
+   */
+  Integer getHeight();
+
+  /**
+   * Returns width of a matrix
+   * @return width
+   */
+  Integer getWidth();
 }
