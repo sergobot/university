ca9fe0a0ddf63e7396b60c8888cfc42b6b6fdd01
Sergey Popov <i@sergobot.com>
2020-10-27 01:24:53 +0300

    [C++] Fix the 5th task
    Use a fixed seed and automate testing

--- year-3/introduction-to-cpp/task-5.cpp
+++ year-3/introduction-to-cpp/task-5.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 #include <functional>
 #include <random>
+#include <iomanip>
 
 int main()
 {
     const long X_LOWER_LIMIT = -1, X_UPPER_LIMIT = 1, Y_LOWER_LIMIT = 0, Y_UPPER_LIMIT = 2;
     const long MAX_AREA = (X_UPPER_LIMIT - X_LOWER_LIMIT) * (Y_UPPER_LIMIT - Y_LOWER_LIMIT);
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    const double TRUE_AREA = 2.38629436111989;
+
+    std::mt19937 gen;
+    gen.seed(11);
     std::uniform_real_distribution<> x_dist(X_LOWER_LIMIT, X_UPPER_LIMIT);
     std::uniform_real_distribution<> y_dist(Y_LOWER_LIMIT, Y_UPPER_LIMIT);
 
@@ -19,17 +22,20 @@ int main()
     std::cout << "Computing area under graph of y = 1 / (2 |x|). x between " << X_LOWER_LIMIT << " and " << X_UPPER_LIMIT
               << ", y between " << Y_LOWER_LIMIT << " and " << Y_UPPER_LIMIT << std::endl;
 
-    size_t iterations;
-    std::cout << "Please enter number of iterations: ";
-    std::cin >> iterations;
-
+    size_t checkpoint = 100, max_iter = 1000000;
     size_t got_under_the_graph = 0;
-    for (size_t i = 0; i < iterations; ++i)
+    for (size_t i = 0; i <= max_iter; ++i)
     {
+        if (i == checkpoint)
+        {
+            double result = (double)got_under_the_graph / checkpoint * MAX_AREA;
+            std::cout << std::setw(9) << checkpoint << " iterations: " << std::setprecision(10) << std::setw(8)
+                      << result << " (error = " << std::setw(8) << std::abs(result - TRUE_AREA) << ")" << std::endl;
+            checkpoint *= 100;
+        }
         double x = x_dist(gen), y = y_dist(gen);
         if (f(x) >= y)
             ++got_under_the_graph;
     }
 
-    std::cout << "Computed area is " << (double)got_under_the_graph / iterations * MAX_AREA << std::endl;
 }
